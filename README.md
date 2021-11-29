e61ae757952be6f19264e0e20b4f944f3850e785
Sydney Sempel <sydneysempel@gmail.com>
2021-11-29 13:12:27 -0800

    yeah

---
+++ README.md
@@ -0,0 +1,13 @@
+PROJECT 3
+
+Basic level project
+
+Sydney Sempel
+Christopher Livingston
+Jeremy Buhain
+
+ISSUES:
+
+CHALLENGES:
+
+
--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <utility>
 
 using namespace std;
+
+uint16_t bitsPerSample;
 
 int main(){
 
@@ -11,30 +16,48 @@ int main(){
 	cout << "---------------------" << endl;
 	cout << "WAV START" << endl;
 	cout << "---------------------" << endl;
-	cout << "1. Begin" << endl;
-	cout << "2. Exit" << endl;
+	cout << "1. Metadata" << endl;
+	cout << "2. Processor" << endl;
+	cout << "3. Exit" << endl;
 	cout << "Enter the corresponding number: " << endl;
 
 	cin << option;
 
-	if (option == 2){
+	if (option == 3){
 		//exit program
 	}
 
 	cout << "Enter filename: " << endl;
-	cin << FILE;
+	cin << FILENAME;
 
-	if (//filename DNE or is not a wav)
+	//opening file
+	FILENAME* wavFile = fopen(file, "r");
+
+	if (wavFile = nullptr)
 	{
 		cout <<  "The file does not exist or is not in the right format, try again!" << endl;
 		//exit program
 	}
 
 	//read file metadata
-	//(1) diplay metadata to user 
+	//filename
+	//sample rate
+	//bits per sample
+	//stereo or mono
+	
+	//diplay metadata to user
+	//filename
+	cout << "The filename name is " << FILENAME << "." << endl;
+	//sample rate
+	cout << "The sample rate is " << sampleRate << "." << endl;
+	//bits per sample
+	cout << "The bits per sample is " << bitsPerSample << "." << endl;
+	//stereo or mono
+	cout << "The file is a " << stereoOrMono << " file." << endl;
+	
 	//If user selects processor option
 		//request output filenames
-		//run processor
+		//run processor - normalization, echo, gain adjustment 
 		//save file
 		//go back to start
 
